8f02a1697a0d19b81b304e13eee9d0ceff77ef6e
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 15:05:12 +0200

    Add project files.

---
+++ Exes and Ohs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exes_and_Ohs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static bool XO(string input)
+        {
+            string inputs = input.ToLower();
+
+            int countX = 0;
+            int countO = 0;
+
+            foreach (var val in inputs)
+            {
+                if (val == 'x')               
+                    countX++;
+                if (val == 'o')
+                    countO++;             
+            }
+
+            return countX == countO;
+        }
+    }
+}
